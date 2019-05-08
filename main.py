49390b6e969e22f8a3cfe2c65a792e2c8ed8ecd8
中野龍一 <170441114@ccalumni.meijo-u.ac.jp>
2019-05-09 02:16:38 +0900

    litle fix

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     num = int(event.message.text)
-    num = random.randrange(num)
+    num = random.randrange(num+1)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(num))
